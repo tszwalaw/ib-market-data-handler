a730c6923f3458020c675fedc469039378f8eeac
Tsz Wa, Law <lawtszwa@gmail.com>
2023-04-08 14:05:58 +0100

    Initial commit

---
+++ src/main/java/com/molamola/automation/ibmarketdatahandler/IbMarketDataHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.molamola.automation.ibmarketdatahandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbMarketDataHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbMarketDataHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/molamola/automation/ibmarketdatahandler/IbMarketDataHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molamola.automation.ibmarketdatahandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbMarketDataHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
